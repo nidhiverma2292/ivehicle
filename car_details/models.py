68489b54f09a053ceb287ba548a523d221ea7235
nidhi2292 <nidhiverma2292@gmail.com>
2014-11-09 02:39:08 +0530

    application added

--- car_details/models.py
+++ car_details/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+class Car(models.Model):
+    car_type = (
+        ('se', 'Sedan'),
+        ('su', 'SUV'),
+        ('ha', 'HashBack')
+    )
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=6, choices=car_type)
+    ac = models.BooleanField(default=True)
+    rent = models.IntegerField()
+    image = models.ImageField(upload_to='/car/')
+
+
--- ivehicle/settings.py
+++ ivehicle/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'car_details',
 )
 
 MIDDLEWARE_CLASSES = (
